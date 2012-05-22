df877a42255d875fb330156c7a4418e8c72fc135
Alister Scott <alister.scott@gmail.com>
2012-05-22 21:26:29 +1000

    First passing scenario

--- features/step_definitions/sudoku_steps.rb
+++ features/step_definitions/sudoku_steps.rb
@@ -7,5 +7,5 @@ When /^I analyse the square group$/ do
 end
 
 Then /^cell (\d+),(\d+) can contain an? (\d+) or an? (\d+)$/ do |row, col, val1, val2|
-	@analysis.possible_values_for(row, col).should == [val1, val2]
+	@analysis.possible_values_for(row, col).should == [val1.to_i, val2.to_i]
 end
--- features/sudoku.feature
+++ features/sudoku.feature
@@ -8,5 +8,5 @@ Scenario: Can work out possible values for a cell in a square group
 	7 _ _
 	"""
 	When I analyse the square group
-	Then cell 2,2 can contain an 8 or a 9
-	And cell 2,3 can contain an 8 or a 9
+	Then cell 2,1 can contain an 8 or a 9
+	And cell 2,2 can contain an 8 or a 9
