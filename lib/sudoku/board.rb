be1d62aac86ad8ada5c6b70e1ef8873d714e4342
Alister Scott <alister.scott@gmail.com>
2012-05-24 20:49:11 +1000

    Added groups

--- lib/sudoku/board.rb
+++ lib/sudoku/board.rb
@@ -4,7 +4,8 @@ class Sudoku::Board
 
   ROW_COUNT = 9
   COL_COUNT = 9
-  attr_reader :cells
+  GROUPS_COUNT = 9
+  attr_reader :cells, :groups
   #attr_reader :row_count, :col_count
 
   def initialize grid
@@ -15,6 +16,11 @@ class Sudoku::Board
       row.each_with_index do |cell, col_index|
         @cells << Sudoku::Cell.new(row_index, col_index, cell)
       end
+    end
+    # Create groups for easy accessibility
+    @groups = []
+    GROUPS_COUNT.times do |index|
+      @groups << @cells.select{ |cell| cell.group == index }
     end
   end
 
