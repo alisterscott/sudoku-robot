3f6707fff836f4803376d4bba2199e18ed7326f3
Alister Scott <alister.scott@gmail.com>
2012-05-24 22:24:21 +1000

    Removed cucumber

--- features/support/env.rb
+++
@@ -1,9 +0,0 @@
-$: << File.dirname(__FILE__)+'/../../lib'
-
-require 'sudoku'
-require 'sudoku/board'
-require 'sudoku/group_analyser'
-require 'sudoku/square_group'
-require 'sudoku/straight_group'
-
-World Sudoku
