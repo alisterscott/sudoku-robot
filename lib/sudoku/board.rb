1fe2cc1dbfbc1d657e7f5b3b697b227e9ce5de3b
Alister Scott <alister.scott@gmail.com>
2012-05-24 20:26:27 +1000

    Added concept of cell

--- lib/sudoku/board.rb
+++ lib/sudoku/board.rb
@@ -1,11 +1,26 @@
 class Sudoku::Board
   include Enumerable
+  include Sudoku
 
-  attr_reader :row_count, :col_count
+  ROW_COUNT = 9
+  COL_COUNT = 9
+  attr_reader :cells
+  #attr_reader :row_count, :col_count
 
-  def initialize board_grid
-    @board_grid = board_grid
-    @row_count = board_grid.length
-    @col_count = board_grid.map(&:length).max
+  def initialize grid
+    @cells = []
+    grid = string_to_grid(grid) unless grid.kind_of? Array
+    grid.each_with_index do |row, row_index|
+      raise "invalid board grid, should be 9 x 9 '#{grid}'" unless row.length == ROW_COUNT and grid.length == COL_COUNT
+      row.each_with_index do |cell, col_index|
+        @cells << Sudoku::Cell.new(row_index, col_index, cell)
+      end
+    end
+  end
+
+  def each
+    @cells.each do |cell|
+      yield cell
+    end
   end
 end
