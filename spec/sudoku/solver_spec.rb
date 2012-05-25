4b8bae335398804953c60425fd437ac7a8032e73
Alister Scott <alister.scott@gmail.com>
2012-05-25 10:14:01 +1000

    solving boards

--- spec/sudoku/solver_spec.rb
+++ spec/sudoku/solver_spec.rb
@@ -19,7 +19,7 @@ describe Sudoku::Solver do
     puts solver.board.to_s
   end
 
-  it 'should not be able to solve a simple board' do
+  it 'should not be able to solve an unsolvable board' do
     board_string = ' 1 _ _ _ _ _ _ _ 2
                      _ _ _ _ _ _ _ _ _
                      _ _ _ _ _ _ _ _ _
@@ -31,6 +31,21 @@ describe Sudoku::Solver do
                      4 _ _ _ _ _ _ _ 5        '
     solver = Sudoku::Solver.new board_string
     solver.should_not be_solved
+    puts solver.board.to_s
+  end
+
+  it 'should be able to solve a difficult board' do
+    board_string = ' _ _ 6 _ 2 _ _ 7 _
+                     2 _ _ 6 _ 4 _ 3 _
+                     _ _ _ _ 9 3 4 _ 2
+                     _ _ 8 _ _ _ _ 4 7
+                     _ _ _ _ _ _ _ _ _
+                     7 6 _ _ _ _ 1 _ _
+                     4 _ 3 8 6 _ _ _ _
+                     _ 7 _ 1 _ 9 _ _ 8
+                     _ 8 _ _ 3 _ 6 _ _        '
+    solver = Sudoku::Solver.new board_string
+    solver.should be_solved
     puts solver.board.to_s
   end
 end
